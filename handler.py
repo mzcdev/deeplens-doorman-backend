8b630132707710b0cf55e0b227d741e60eca3e25
nalbam <me@nalbam.com>
2020-06-04 14:25:59 +0900

    get_history Limit 20

--- handler.py
+++ handler.py
@@ -272,8 +272,7 @@ def get_history(user_id):
         visited = int(round(time.time() * 1000)) - (30 * 60 * 60 * 1000)
 
         res = tbl.query(
-            KeyConditionExpression=Key("user_id").eq(user_id)
-            & Key("visited").gte(visited),
+            KeyConditionExpression=Key("user_id").eq(user_id),
             IndexName="history_index",
             ScanIndexForward=False,  # true = asc, false = desc
             Limit=20,
