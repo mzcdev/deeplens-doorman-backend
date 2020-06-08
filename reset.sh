8edd8b4aa2905337d1b13a82cd2b2dbef158c3cf
nalbam <me@nalbam.com>
2020-06-08 17:45:47 +0900

    S3Bucket

--- reset.sh
+++ reset.sh
@@ -3,3 +3,5 @@
 aws rekognition delete-collection --collection-id ${STORAGE_NAME} --region ${AWSREGION} | jq .
 
 aws rekognition create-collection --collection-id ${STORAGE_NAME} --region ${AWSREGION} | jq .
+
+sls deploy
