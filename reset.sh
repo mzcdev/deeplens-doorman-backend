6316bbce3e4cfea8383d87cd66ef439ffc8a4916
nalbam <me@nalbam.com>
2020-06-08 17:49:17 +0900

    echo

--- reset.sh
+++ reset.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+echo "aws rekognition delete-collection --collection-id ${STORAGE_NAME} --region ${AWSREGION}"
 aws rekognition delete-collection --collection-id ${STORAGE_NAME} --region ${AWSREGION} | jq .
 
+echo "aws rekognition create-collection --collection-id ${STORAGE_NAME} --region ${AWSREGION}"
 aws rekognition create-collection --collection-id ${STORAGE_NAME} --region ${AWSREGION} | jq .
 
+echo "sls deploy"
 sls deploy
