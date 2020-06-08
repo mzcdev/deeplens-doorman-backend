d02a4de5af4b2912c71af3b5474c3791032a965d
nalbam <me@nalbam.com>
2020-06-08 17:31:33 +0900

    latest_index

---
+++ reset.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+aws rekognition delete-collection --collection-id ${STORAGE_NAME} --region ${AWSREGION} | jq .
+
+aws rekognition create-collection --collection-id ${STORAGE_NAME} --region ${AWSREGION} | jq .
